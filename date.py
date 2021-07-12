27c12e72149346afa29759939b413e2b4e028f33
Vanshika <vanshikag0111@gmail.com>
2021-07-12 19:07:58 +0530

    displaying current date via AI

---
+++ date.py
@@ -0,0 +1,22 @@
+import pyttsx3
+
+# to extract date
+import datetime
+
+engine = pyttsx3.init()
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def date():
+    year = int ( datetime.datetime.now().year )
+    month = int ( datetime.datetime.now().month )
+    date = int ( datetime.datetime.now().day )
+
+    speak("Today's date is ")
+    speak(date)
+    speak(month)
+    speak(year)
+
+date()
