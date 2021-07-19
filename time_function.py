490c4f864e44377909012c79d7ddad4cb40d6af6
Vanshika <vanshikag0111@gmail.com>
2021-07-19 22:37:15 +0530

    to convey current time to user

---
+++ time_function.py
@@ -0,0 +1,22 @@
+# to displat the current time
+
+import pyttsx3
+
+# to extract time 
+import datetime
+
+engine = pyttsx3.init()
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def time():
+    Time = datetime.datetime.now().strftime("%H:%M:%S")
+    # .strftime will extract the current time from module
+    # speak("The time is {Time}")
+    speak("Current time is ")
+    speak(Time)
+    # speak(Time)
+
+time()
