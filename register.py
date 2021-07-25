33307dbcd5e5d332ae5c46d64b67af14033777df
Vanshika <vanshikag0111@gmail.com>
2021-07-25 12:04:32 +0530

    how to register for domain

---
+++ register.py
@@ -0,0 +1,14 @@
+# to register domain for free
+
+"""
+1. search "github student developer pack"
+2. go to site "https://education.github.com/pack"
+3. click on "get you pack"
+4. scroll down and select "get student benefits"
+5. login with your GitHub account
+6. add the email address of respective school/college
+7. press continue
+8. wait for verified mail of domain registration
+9. wait for verified registration email
+10. once verified, select the domain you want to get
+"""
