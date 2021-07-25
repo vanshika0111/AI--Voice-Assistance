325fe1ff4833728eec115737bb2603980ee4ce38
Vanshika <vanshikag0111@gmail.com>
2021-07-25 12:05:21 +0530

    how to take screenshots of the screen

---
+++ ss.py
@@ -0,0 +1,16 @@
+# to take screenshots of the screen
+
+# install the module externally -->
+# pip install pyautogui
+
+import pyautogui
+
+# definition for taking screenshot
+# def screenshot():
+#     img = pyautogui.screenshot()
+#     img.save("E:\photooooo\ss.png")
+
+# code for running
+# if "screenshot" in query:
+#             screenshot()
+#             speak("Done!")
