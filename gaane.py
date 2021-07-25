a9fb26ea70912e4c2e9f20706c466c93f04f19fd
Vanshika <vanshikag0111@gmail.com>
2021-07-25 11:50:01 +0530

    to play songs from os

---
+++ gaane.py
@@ -0,0 +1,11 @@
+# to play songs from the system
+
+import os
+
+# songs directory path in my system --> E:\playlist
+
+# below is the code:
+# if "play songs" in query:
+#             songs_directory = "E:\playlist"
+#             songs = os.listdir(songs_directory)   --> gives list of songs
+#             os.startfile(os.path.join(songs_directory, songs[0]))   --> starts playing songs from zero index
