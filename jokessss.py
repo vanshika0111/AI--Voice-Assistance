8e8e9b037eb4e1a5fbad0d8afb12aeb5cac918b7
Vanshika <vanshikag0111@gmail.com>
2021-07-25 11:58:32 +0530

    to convey the jokes

---
+++ jokessss.py
@@ -0,0 +1,14 @@
+# to tell jokes
+
+# pip install pyjokes
+
+import pyjokes
+
+# definition
+# def jokes():
+#     speak(pyjokes.get_joke())
+
+# code
+# elif "joke" in query:
+#             jokes()
+
