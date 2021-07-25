fc1649e4081161c8c1cf94eec0d10ed7ea039ffb
Vanshika <vanshikag0111@gmail.com>
2021-07-25 12:11:33 +0530

    to log out/shut-down/restart the system

---
+++ window_operation.py
@@ -0,0 +1,19 @@
+# to log-out, restart and shut down the system
+
+import os
+
+# this is built-in library 
+# no need to install this module
+
+# logout --> logs out the user from the system
+# shutdown --> shut-down the system
+# restart --> restarts the system
+
+# below is the code:
+# if "logout" in query:
+#             os.system("shutdown - l")
+# elif "shutdown" in query:
+#             os.system("shutdown /s /t 1")
+# elif "restart" in query:
+#             os.system("shutdown /r /t 1")
+
