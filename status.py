71ce55158b572a19e2e37fd47259ef738b61ada5
Vanshika <vanshikag0111@gmail.com>
2021-07-25 12:06:49 +0530

    to check the battery status of CPU

---
+++ status.py
@@ -0,0 +1,20 @@
+# to display/check the CPU battery 
+
+#install via pip terminal -->
+# pip install psutil
+
+import psutil
+
+# definition 
+# def cpu():
+#     usage = str(psutil.cpu_percent())
+#     speak("CPU is at ")
+#     speak(usage)
+
+#     battery = psutil.sensors_battery
+#     speak("Battery is at ")
+#     speak(battery.percent)
+
+# code
+# if "cpu" in query:
+#             cpu()
